34ba3fc82e29ac5407597ad6994608a35f7fa88f
Șular Paul <sular_paul@yahoo.com>
2023-05-16 23:19:16 +0300

    simplified build excludes

--- next.config.js
+++ next.config.js
@@ -7,22 +7,7 @@ const isDev = process.env.NODE_ENV !== "production";
 const withPWA = withPWAInit({
   dest: "public",
   // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
-  buildExcludes: [
-    ({ asset }) => {
-      if (
-        asset.name.startsWith("server/") ||
-        asset.name.match(
-          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
-        )
-      ) {
-        return true;
-      }
-      if (isDev && !asset.name.startsWith("static/runtime/")) {
-        return true;
-      }
-      return false;
-    },
-  ],
+  buildExcludes: ["app-build-manifest.json"],
 });
 
 const generateAppDirEntry = (entry) => {
